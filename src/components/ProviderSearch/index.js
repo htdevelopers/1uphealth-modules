1a3996041cccf54920a041ea95f639d2b4fefc3d
MChrustowski <chrustowski.m@gmail.com>
2019-05-02 12:01:07 +0200

    Update catalog structure

---
+++ src/components/ProviderSearch/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './styles/index.css';
+
+class ProviderSearch extends Component {
+  render() {
+    return <div>ProviderSearch</div>;
+  }
+}
+
+export default ProviderSearch;
