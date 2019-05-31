42dfef540ed88791a9faae4ddb9e4c5641f615ff
MChrustowski <chrustowski.m@gmail.com>
2019-05-31 16:29:40 +0200

    Add input & list view

--- examples/create-react-app/src/App.js
+++ examples/create-react-app/src/App.js
@@ -3,16 +3,15 @@ import './App.css';
 import Api from '@1uphealth-temp/api';
 import ProviderSearch from '@1uphealth-temp/provider-search';
 
-const api = new Api("elo");
+import '@1uphealth-temp/provider-search/dist/index.css';
+
+const api = new Api('elo');
 
 function App() {
   return (
     <div className="App">
-      <ProviderSearch.Base
-        api={api}
-      >
+      <ProviderSearch.Base api={api}>
         <ProviderSearch.Input />
-        elo
         <ProviderSearch.List />
       </ProviderSearch.Base>
     </div>
