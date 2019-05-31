69e462b327bb1e57e221c122ed3032a29c2a8d13
Kacper Perzankowski <perzankowski.kacper@gmail.com>
2019-05-31 16:31:24 +0200

    extend documentation

--- packages/1uphealth-api/src/api-sdk.ts
+++ packages/1uphealth-api/src/api-sdk.ts
@@ -16,7 +16,10 @@ import Validator from './validator';
  * @export
  * @class OneUpApiSDK
  * @implements {Config}
- * @implements {UserManagementScope}
+ * @implements {Auth}
+ * @implements {Scope.UserManagement}
+ * @implements {Scope.UI}
+ * @implements {Scope.Connect}
  */
 export default class OneUpApiSDK
   implements Config, Auth, Scope.UserManagement, Scope.UI, Scope.Connect {
@@ -82,10 +85,15 @@ export default class OneUpApiSDK
   }
 
   /**
-   * GET /user-management/v1/user
+   * **GET /user-management/v1/user** \
+   * Get the list of all the users that exist inside your 1up Developer Application,
+   * with the option of filtering by specific users
    *
    * @param {Method.GetUsers} [parameters]
+   * **oneup_user_id** — string \
+   * **app_user_id** — string
    * @returns {Promise<HttpClientResponse>}
+   * An array of user objects
    * @memberof OneUpApiSDK
    */
   async getUsers(parameters?: Method.GetUsers): Promise<HttpClientResponse> {
@@ -107,7 +115,9 @@ export default class OneUpApiSDK
   }
 
   /**
-   * POST /user-management/v1/user
+   * **POST /user-management/v1/user** \
+   * Will cause a user to be created with the attributes passed in the request body. \
+   * The request won't fail if the user already exists but rather will return the user data but with
    *
    * @param {Method.CreateUser} payload
    * @returns {Promise<HttpClientResponse>}
@@ -126,10 +136,13 @@ export default class OneUpApiSDK
   }
 
   /**
-   * PUT /user-management/v1/user
+   * **PUT /user-management/v1/user** \
+   * Can be used to modify an existing user object. It is possible to modify the **app_user_id**,
+   * but the **oneup_user_id** is assigned when the user is created and cannot be changed.
    *
    * @param {Method.UpdateUser} payload
    * @returns {Promise<HttpClientResponse>}
+   * Will return the new user object
    * @memberof OneUpApiSDK
    */
   async updateUser(payload: Method.UpdateUser): Promise<HttpClientResponse> {
@@ -146,10 +159,15 @@ export default class OneUpApiSDK
   }
 
   /**
-   * POST /user-management/v1/user/auth-code
+   * **POST /user-management/v1/user/auth-code** \
+   * A backend app can use this endpoint to get a new authorization code for a user. \
+   * Note that this endpoint should not be called in a browser context because \
+   * it would require exposing your app's secret key to users.
    *
    * @param {Method.GenerateUserAuthCode} payload
    * @returns {Promise<HttpClientResponse>}
+   * Returns an access token and a refresh token,
+   * which can be used to authenticate requests made on behalf of the user.
    * @memberof OneUpApiSDK
    */
   async generateUserAuthCode(payload: Method.GenerateUserAuthCode): Promise<HttpClientResponse> {
@@ -164,10 +182,16 @@ export default class OneUpApiSDK
   }
 
   /**
-   * GET /connect/marketplace
-   * returns HTML
+   * **GET /connect/marketplace** \
+   * A common pattern is to ask the user to connect data from their health system to the app. \
+   * This endpoint returns a simple html page that can be used as a starting point for walking \
+   * the user through the data connect flow. For more details, \
+   * read about the https://1up.health/dev/doc/connect-health-data-iframe.
    *
    * @returns {Promise<HttpClientResponse>}
+   * This endpoint returns an html page and inline css that, when rendered, results in a UI for \
+   * selecting which health care system the user would like to connect. The most common use of \
+   * this is to render this endpoint in an iframe within the developers own app.
    * @memberof OneUpApiSDK
    */
   async getHealthSystemPickerIFrame(): Promise<HttpClientResponse> {
@@ -181,10 +205,21 @@ export default class OneUpApiSDK
   }
 
   /**
-   * GET /connect/system/provider
+   * **GET /connect/system/provider** \
+   * Used to run a text search on health systems, often for the purpose of allowing the user \
+   * to find their health system's authorization portal.
    *
-   * @param {Method.SearchConnectProvider} { query: q }
+   * @param {Method.SearchConnectProvider}
+   * { query: string; } \
+   * This is how you specify the text to search for. \
+   * It can be a doctor's name, clinic, hospital, or address.
    * @returns {Promise<HttpClientResponse>}
+   * The result will return a bundle of FHIR Organizations which contain the 1upHealth \
+   * health system id and an extension with the logo of that org or health system. \
+   * You can use that to direct the patient to the correct connect api url so the patient \
+   * can authorize sharing of their medical data. \
+   * See the official hl7 FHIR docs (https://www.hl7.org/fhir/bundle.html) \
+   * for more information on how a FHIR bundle is structured.
    * @memberof OneUpApiSDK
    */
   async searchConnectProvider(
