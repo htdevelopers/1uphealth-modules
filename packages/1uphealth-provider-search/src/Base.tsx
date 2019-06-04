4bcdf042c9fb38dd2f73e0cc91badd22832f9b4f
MChrustowski <chrustowski.m@gmail.com>
2019-06-04 12:09:56 +0200

    Add getProviders method

--- packages/1uphealth-provider-search/src/Base.tsx
+++ packages/1uphealth-provider-search/src/Base.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import Api from '@1uphealth-temp/api/src/api-sdk';
 
 interface Props {}
 
@@ -7,6 +8,14 @@ interface State {}
 class Base extends React.Component<Props, State> {
   public constructor(props: Props) {
     super(props);
+  }
+
+  public getProviders = () => {
+    const api = new Api({ clientId: 'test', clientSecret: 'test' });
+
+    api.searchConnectProvider({ query: 'john' })
+      .then(r => console.log(r))
+      .catch(error => console.log(error));
   }
 
   render() {
